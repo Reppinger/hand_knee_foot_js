01ef4d49103971a380773d654471416ece758fd2
Randy Eppinger <randy.eppinger@leandog.com>
2016-01-06 16:11:41 -0500

    Added users page.

--- kit-es2016/src/app.js
+++ kit-es2016/src/app.js
@@ -1,11 +1,12 @@
 export class App {
   configureRouter(config, router) {
-    config.title = 'Aurelia';
+    config.title = 'Aurelia by committee'
     config.map([
-      { route: [''], name: 'welcome', moduleId: './welcome', nav: false, title:'Home' },
-      { route: ['welcome'], name: 'welcome', moduleId: './welcome', nav: true, title:'Welcome' }
-    ]);
+      { route: '',        name: 'welcome', moduleId: './welcome', nav: false, title:'Home' },
+      { route: 'welcome', name: 'welcome', moduleId: './welcome', nav: true,  title:'Welcome' },
+      { route: 'users',   name: 'users',   moduleId: './users',   nav: true,  title:'Github Users' }
+    ])
 
-    this.router = router;
+    this.router = router
   }
 }
