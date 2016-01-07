b53c9dd7d5f2927fa38dff165dae005a83c5f9d5
Randy Eppinger <randy.eppinger@leandog.com>
2016-01-07 09:29:33 -0500

    Added vertical child router just to demonstrate you can have multiple routers.

--- kit-es2016/src/app.js
+++ kit-es2016/src/app.js
@@ -2,9 +2,10 @@ export class App {
   configureRouter(config, router) {
     config.title = 'Aurelia by committee'
     config.map([
-      { route: '',        name: 'welcome', moduleId: './welcome', nav: false, title:'Home' },
-      { route: 'welcome', name: 'welcome', moduleId: './welcome', nav: true,  title:'Welcome' },
-      { route: 'users',   name: 'users',   moduleId: './users',   nav: true,  title:'Github Users' }
+      { route: '',             name: 'welcome',      moduleId: './welcome',      nav: false, title:'Home' },
+      { route: 'welcome',      name: 'welcome',      moduleId: './welcome',      nav: true,  title:'Welcome' },
+      { route: 'users',        name: 'users',        moduleId: './users',        nav: true,  title:'Github Users' },
+      { route: 'child-router', name: 'child-router', moduleId: './child-router', nav: true,  title:'Child Router' }
     ])
 
     this.router = router
